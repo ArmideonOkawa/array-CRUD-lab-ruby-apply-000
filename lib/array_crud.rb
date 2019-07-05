f4f422c47958951d77c515c5794c4dc7fdc40bae
jeremy perry <jeremy.m.perry@gmail.com>
2019-07-05 06:49:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ["Box", "Cabinet", "Drawer", "Closet", "Chair"]
+  ["Box", "Cabinet", "Drawer", "Closet", "arrays!"]
   
 end
 
